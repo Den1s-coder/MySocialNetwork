8a554ed350dc4411081e5b15c63a8e2317a2ae59
Denis Katasonov <diarusfoxs@gmail.com>
2025-06-26 18:05:05 +0300

    Add comment Entity

---
+++ SocialNetwork.Domain/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public User Author { get; set; }
+
+        public Comment(int id, string text, User user)
+        {
+            Id = id;
+            Text = text;
+            Author = user;
+        }
+    }
+}
