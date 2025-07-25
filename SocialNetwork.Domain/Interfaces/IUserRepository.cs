849668c53aa3a4779958a0f84cfadfd594e64fb7
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-25 20:00:37 +0300

    Add interfaces for Repositories

---
+++ SocialNetwork.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialNetwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        
+    }
+}
