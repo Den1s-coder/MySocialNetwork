68832b505d7210fe10c10b40c033c3b0e00029a8
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-07 10:28:37 +0300

    chore: add Async

--- SocialNetwork.Infrastructure/Repos/UserRepository.cs
+++ SocialNetwork.Infrastructure/Repos/UserRepository.cs
@@ -40,7 +40,7 @@ namespace SocialNetwork.Infrastructure.Repos
             return await _context.Users.AsNoTracking().ToListAsync();
         }
 
-        public Task<User?> GetById(Guid id)
+        public Task<User?> GetByIdAsync(Guid id)
         {
             return _context.Users.FirstOrDefaultAsync(u => u.Id == id);
         }
