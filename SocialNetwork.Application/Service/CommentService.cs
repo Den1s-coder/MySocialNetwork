467628c1e1cb25307a882649cea2630893b78856
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-18 10:47:50 +0300

    feat: Init Services

---
+++ SocialNetwork.Application/Service/CommentService.cs
@@ -0,0 +1,34 @@
+﻿using SocialNetwork.Application.DTO;
+using SocialNetwork.Domain.Entities;
+using SocialNetwork.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.Service
+{
+    public class CommentService : ICommentService
+    {
+        public Task BanComment(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(CreateCommentDto commentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Comment>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comment?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SocialNetwork.Application/Service/UserService.cs
@@ -0,0 +1,44 @@
+﻿using SocialNetwork.Application.DTO;
+using SocialNetwork.Application.Interfaces;
+using SocialNetwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.Service
+{
+    public class UserService : IUserService
+    {
+        public Task BanUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(CreateUserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUserIdsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
