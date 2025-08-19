be83a7b6c5eac9fa05d936b2645fa3f76be4bf39
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-19 11:59:37 +0300

    chore: migration for add IsBanned

---
+++ SocialNetwork.Infrastructure/Migrations/20250819085614_IsBannedUpdate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsBannedUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBanned",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBanned",
+                table: "Posts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBanned",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBanned",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsBanned",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "IsBanned",
+                table: "Comments");
+        }
+    }
+}
