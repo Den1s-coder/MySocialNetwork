91c8b7d257a10405de98a00a4301bbeddb102251
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-21 12:05:06 +0300

    feat: add User repositry interface
    - follows the generic repository.
    - add search user by Email and UserName.

---
+++ SocialNetwork.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using SocialNetwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Domain.Interfaces
+{
+    public interface IUserRepository : IGenerycRepository<User>
+    {
+        public Task<User?> GetByEmailAsync(string Email);
+
+        public Task<User?> GetByUserNameAsync(string UserName);
+
+    }
+}
