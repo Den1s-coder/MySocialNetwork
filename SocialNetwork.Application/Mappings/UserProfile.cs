fa3c57ee8ce67f13782758fcd42c020ab3455565
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-21 12:05:36 +0300

    feat: Add mapping for User and CreateUserDto

---
+++ SocialNetwork.Application/Mappings/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace SocialNetwork.Application.Mappings
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<Domain.Entities.User, DTO.CreateUserDto>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ReverseMap();
+        }
+    }
+}
