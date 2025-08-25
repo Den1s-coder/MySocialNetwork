197fef9a53c7360df6372f9c92c6ebdf27e3056e
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-25 10:30:41 +0300

    feat: init Comment controller

---
+++ SocialNetwork.API/Controllers/CommentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            
+        }
+
+        [HttpGet("{id}")]
+        public Task<IActionResult> Get(int id)
+        {
+            
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public Task<IActionResult> Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public Task<IActionResult> Delete(int id)
+        {
+        }
+    }
+}
