fc79fa65d953e2ce5cdd7a160a44b899bbee37d6
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-25 10:30:54 +0300

    feat: init post controller

---
+++ SocialNetwork.API/Controllers/PostController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Domain.Entities;
+
+namespace SocialNetwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public Task<IActionResult> GetAllPosts()
+        {
+            return ;
+        }
+
+        [HttpGet("{id}")]
+        public Task<IActionResult> GetById(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public Task<IActionResult> Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public Task<IActionResult> Delete(int id)
+        {
+        }
+    }
+}
