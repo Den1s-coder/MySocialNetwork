bc9c991757c51ea927ae98787c501bfec9350b1d
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-25 12:39:11 +0300

    chore: DI

--- SocialNetwork.Api/Program.cs
+++ SocialNetwork.Api/Program.cs
@@ -1,11 +1,26 @@
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using SocialNetwork.Application.Interfaces;
+using SocialNetwork.Application.Mappings;
+using SocialNetwork.Application.Service;
+using SocialNetwork.Domain.Entities;
+using SocialNetwork.Domain.Interfaces;
 using SocialNetwork.Infrastructure;
+using SocialNetwork.Infrastructure.Repos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(typeof(CommentProfile), typeof(PostProfile));
+
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IGenerycRepository<Post>, PostRepository>();
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddScoped<IPostService, PostService>();
 
 builder.Services.AddDbContext<SocialDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
