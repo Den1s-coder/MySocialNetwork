1a1cd7f2fa7ddbf126752f3f07b68ece6db73fb3
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-26 10:50:10 +0300

    feat: Add global exception handle

---
+++ SocialNetwork.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿namespace SocialNetwork.API.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ArgumentNullException ex)
+            {
+                _logger.LogWarning(ex, "Invalid input");
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsync(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                _logger.LogWarning(ex, "Entity not found");
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsync(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception");
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                await context.Response.WriteAsync("An error occurred");
+            }
+        }
+    }
+}
