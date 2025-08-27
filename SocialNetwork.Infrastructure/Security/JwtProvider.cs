bd8d79db743dad3d918b28682273da7e18c3556b
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-27 10:41:36 +0300

    feat: Realise JwtProvider
    - Realise JwtProvider
    - Add JWT nuget package

---
+++ SocialNetwork.Infrastructure/Security/JwtProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SocialNetwork.Domain.Entities;
+using SocialNetwork.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Infrastructure.Security
+{
+    public class JwtProvider : IJwtProvider
+    {
+        private readonly JwtOptions _options;
+        public JwtProvider(IOptions<JwtOptions> options) 
+        { 
+            _options = options.Value;
+        }
+
+        public string Generate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("name", user.Name)
+            };
+
+            var signingKey = new SigningCredentials( 
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)), 
+                SecurityAlgorithms.HmacSha256);
+
+            var jwtToken = new JwtSecurityToken(
+                expires: DateTime.UtcNow.AddHours(_options.ExpiresHours),
+                claims: claims,
+                signingCredentials: signingKey);
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+        }
+    }
+}
