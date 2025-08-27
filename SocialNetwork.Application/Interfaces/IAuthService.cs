ae603bbc8926c9e3255db6539b2657b6f43c7d3c
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-27 09:29:22 +0300

    refactor: separate Create User from UserService to AuthService

---
+++ SocialNetwork.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using SocialNetwork.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task RegisterAsync(RegisterDto userDto);
+        public Task LoginAsync(LoginDto loginDto);
+    }
+}
