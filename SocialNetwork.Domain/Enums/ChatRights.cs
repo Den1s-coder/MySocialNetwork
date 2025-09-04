7ffee735c8a7cd060b6a8954265d16d09a1bbc3f
Denis Katasonov <diarusfoxs@gmail.com>
2025-09-04 09:54:30 +0300

    feat: add chat enums

---
+++ SocialNetwork.Domain/Enums/ChatRights.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Domain.Enums
+{
+    [Flags]
+    public enum ChatRights
+    {
+        None = 0,
+        Read = 1,
+        Write = 2,
+        Update = 4,
+        Delete = 8
+    }
+}
