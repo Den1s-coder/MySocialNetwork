a8ae55228ab3909bcae2a9a0558cecd78ae5aa65
Denis Katasonov <diarusfoxs@gmail.com>
2025-09-05 09:14:22 +0300

    feat: add Chat EF Configurations

---
+++ SocialNetwork.Infrastructure/Configurations/ChatConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialNetwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Infrastructure.Configurations
+{
+    public class ChatConfiguration: IEntityTypeConfiguration<Chat>
+    {
+        public void Configure(EntityTypeBuilder<Chat> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Title)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.CreatedAt)
+                .IsRequired();
+
+            builder.HasMany(c => c.Messages)
+                   .WithOne(m => m.Chat)
+                   .HasForeignKey(m => m.ChatId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(c => c.UserChats)
+                   .WithOne(uc => uc.Chat)
+                   .HasForeignKey(uc => uc.ChatId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
---
+++ SocialNetwork.Infrastructure/Configurations/MessageConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialNetwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Infrastructure.Configurations
+{
+    public class MessageConfiguration: IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasKey(m => m.Id);
+
+            builder.Property(m => m.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(m => m.Content)
+                .HasMaxLength(1000)
+                .IsRequired();
+
+            builder.Property(m => m.SentAt)
+                .IsRequired();
+
+            builder.HasOne(m => m.Chat)
+                   .WithMany(c => c.Messages)
+                   .HasForeignKey(m => m.ChatId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(m => m.Sender)
+                   .WithMany(u => u.Messages)
+                   .HasForeignKey(m => m.SenderId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }    
+}
