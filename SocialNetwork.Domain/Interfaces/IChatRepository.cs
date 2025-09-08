e99e5afbcd9af667726a1a4063f4084ecb680949
Denis Katasonov <diarusfoxs@gmail.com>
2025-09-08 09:38:21 +0300

    chore: add Interfaces for Chat and Message repository

---
+++ SocialNetwork.Domain/Interfaces/IChatRepository.cs
@@ -0,0 +1,15 @@
+﻿using SocialNetwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Domain.Interfaces
+{
+    public interface IChatRepository: IGenerycRepository<Chat>
+    {
+        public Task<IEnumerable<Chat>> GetChatsByUserIdAsync(Guid userId);
+        public Task<Chat?> GetChatWithMessagesAsync(Guid chatId);
+    }
+}
