52a7fac02b58c4b575fdccbfd7e2b18ea5f84a2d
Denis Katasonov <diarusfoxs@gmail.com>
2025-09-09 09:31:10 +0300

    feat: Add Interfaces for Chat and Message services (#27)
    Связанные рабочие элементы: #2

---
+++ SocialNetwork.Application/Interfaces/IChatService.cs
@@ -0,0 +1,19 @@
+﻿using SocialNetwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.Interfaces
+{
+    public interface IChatService
+    {
+        public Task<Chat> CreatePrivateChatAsync(Guid userId1, Guid userId2);
+        public Task<Chat> CreateGroupChatAsync(string title, Guid ownerId);
+        public Task<Chat> CreateChannelChatAsync(string title, Guid ownerId);
+        public Task AddUserToChatAsync(Guid chatId, Guid userId);
+        public Task RemoveUserFromChatAsync(Guid chatId, Guid userId);
+        public Task ChangeUserRoleInChatAsync(Guid chatId, Guid userId, int newRole);
+    }
+}
