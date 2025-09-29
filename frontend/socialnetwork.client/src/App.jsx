bcc55603a36c72eddf63acae8ff08435dff25417
Denis Katasonov <diarusfoxs@gmail.com>
2025-09-29 10:03:24 +0300

    chore: add routes

--- frontend/socialnetwork.client/src/App.jsx
+++ frontend/socialnetwork.client/src/App.jsx
@@ -3,11 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Home />
+      <>
+          <BrowserRouter>
+              <nav style={{ display: 'flex', gap: 12, padding: 12 }}>
+                  <Link to="/">Головна</Link>
+                  <Link to="/login">Увійти</Link>
+                  <Link to="/register">Регистрація</Link>
+              </nav>
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
+              </Routes>
+          </BrowserRouter>
     </>
   )
 }
