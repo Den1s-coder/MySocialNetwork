9c0fea3a9e5d4c75c21103e7e517258433fd0361
Denis Katasonov <diarusfoxs@gmail.com>
2025-09-29 10:21:37 +0300

    feat: add NavBar

--- frontend/socialnetwork.client/src/App.jsx
+++ frontend/socialnetwork.client/src/App.jsx
@@ -1,26 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
+import NavBar from './components/NavBar.jsx';
 
 function App() {
   return (
       <>
           <BrowserRouter>
-              <nav style={{ display: 'flex', gap: 12, padding: 12 }}>
-                  <Link to="/">Головна</Link>
-                  <Link to="/login">Увійти</Link>
-                  <Link to="/register">Регистрація</Link>
-              </nav>
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-              </Routes>
+              <NavBar />
+              <div style={{ paddingTop: 56 }}>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                  </Routes>
+              </div>
           </BrowserRouter>
     </>
   )
