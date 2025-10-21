13083f3f31464891b50594cffb3921c4f6a91c47
Denis Katasonov <diarusfoxs@gmail.com>
2025-10-21 20:22:14 +0300

    feat: update useAuth hook

--- frontend/socialnetwork.client/src/hooks/useAuth.jsx
+++ frontend/socialnetwork.client/src/hooks/useAuth.jsx
@@ -1,8 +1,40 @@
 import { useState, useEffect } from 'react';
 
+const getUserNameFromToken = (token) => {
+    try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        const payload = JSON.parse(jsonPayload);
+        return payload['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
+    } catch (error) {
+        console.error('Помилка декодування токена:', error);
+        return null;
+    }
+};
+
+const getUserIdFromToken = (token) => {
+    try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        const payload = JSON.parse(jsonPayload);
+        return payload['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid'];
+    } catch (error) {
+        console.error('Помилка декодування токена:', error);
+        return null;
+    }
+};
+
 export function useAuth() {
     const [token, setToken] = useState(localStorage.getItem('token'));
     const [isAuthenticated, setIsAuthenticated] = useState(Boolean(token));
+    const [currentUserId, setCurrentUserId] = useState(null);
+    const [currentUserName, setCurrentUserName] = useState(null);
 
     useEffect(() => {
         const handleStorageChange = () => {
@@ -15,11 +47,31 @@ export function useAuth() {
         return () => window.removeEventListener('storage', handleStorageChange);
     }, []);
 
+    useEffect(() => {
+        if (token) {
+            const userId = getUserIdFromToken(token);
+            const userName = getUserNameFromToken(token);
+            setCurrentUserId(userId);
+            setCurrentUserName(userName);
+        } else {
+            setCurrentUserId(null);
+            setCurrentUserName(null);
+        }
+    }, [token]);
+
     const logout = () => {
         localStorage.removeItem('token');
         setToken(null);
         setIsAuthenticated(false);
+        setCurrentUserId(null);
+        setCurrentUserName(null);
     };
 
-    return { token, isAuthenticated, logout };
+    return {
+        token,
+        isAuthenticated,
+        currentUserId,
+        currentUserName,
+        logout
+    };
 }
