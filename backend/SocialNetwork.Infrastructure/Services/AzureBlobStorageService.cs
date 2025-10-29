2cc18540eed317b97f3a39bb98deed9080289180
Denis Katasonov <diarusfoxs@gmail.com>
2025-10-29 19:13:02 +0200

    feat: add AzureBlobStorageService (#69)

---
+++ backend/SocialNetwork.Infrastructure/Services/AzureBlobStorageService.cs
@@ -0,0 +1,54 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using SocialNetwork.Domain.Interfaces;
+
+namespace SocialNetwork.Infrastructure.Services
+{
+    public class AzureBlobStorageService : ICloudStorageService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _containerName;
+
+        public AzureBlobStorageService(BlobServiceClient blobServiceClient, string containerName)
+        {
+            _blobServiceClient = blobServiceClient;
+            _containerName = containerName;
+        }
+
+        public async Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            var uploadOptions = new BlobUploadOptions
+            {
+                HttpHeaders = new BlobHttpHeaders
+                {
+                    ContentType = contentType
+                }
+            };
+        
+            await blobClient.UploadAsync(fileStream, uploadOptions);
+
+            return blobClient.Uri.ToString();
+        }
+
+        public Task DeleteFileAsync(string fileName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            return blobClient.DeleteIfExistsAsync();
+        }
+
+        public string GetFileUrl(string fileName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
