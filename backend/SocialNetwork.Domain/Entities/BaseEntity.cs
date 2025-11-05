4a585af4568dc8bd12eddfd0dedb294431570ffa
Denis Katasonov <diarusfoxs@gmail.com>
2025-11-05 12:34:14 +0200

    feat: add Friendship entity (#71)

--- backend/SocialNetwork.Domain/Entities/BaseEntity.cs
+++ backend/SocialNetwork.Domain/Entities/BaseEntity.cs
@@ -9,7 +9,7 @@ namespace SocialNetwork.Domain.Entities
     public abstract class BaseEntity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
