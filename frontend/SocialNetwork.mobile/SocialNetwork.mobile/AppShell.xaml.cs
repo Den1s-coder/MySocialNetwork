b2f4fb7d23c782559a81886791162b243f594dca
Den1s-coder <diarusfocs@gmail.com>
2025-11-05 14:08:17 +0200

    feat: Init xamarin client (#66)

---
+++ frontend/SocialNetwork.mobile/SocialNetwork.mobile/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SocialNetwork.mobile.ViewModels;
+using SocialNetwork.mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SocialNetwork.mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ frontend/SocialNetwork.mobile/SocialNetwork.mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SocialNetwork.mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SocialNetwork.mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
