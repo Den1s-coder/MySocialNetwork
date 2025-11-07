501eee322363031386c337b4d1b530d7858858f7
Den1s-coder <diarusfocs@gmail.com>
2025-11-07 15:49:07 +0200

    chore: add DTO for mobile client

---
+++ frontend/SocialNetwork.mobile/SocialNetwork.mobile/DTO/PostDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.mobile.DTO
+{
+    public class PostDto
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public string ImageUrl { get; set; }
+        public string UserName { get; set; }
+        public List<CommentDto> Comments { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsBanned { get; set; }
+    }
+}
