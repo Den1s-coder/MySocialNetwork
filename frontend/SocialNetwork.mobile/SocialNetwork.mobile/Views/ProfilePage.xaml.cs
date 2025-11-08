69c8c415051bc33db3a8e703e6793e5023f41a68
Den1s-coder <diarusfoxs@gmail.com>
2025-11-08 20:55:17 +0200

    feat: Profile page

---
+++ frontend/SocialNetwork.mobile/SocialNetwork.mobile/Views/ProfilePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using SocialNetwork.mobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SocialNetwork.mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        ProfilePageViewModel _vm;
+        public ProfilePage()
+        {
+            InitializeComponent();
+            BindingContext = _vm = new ProfilePageViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (_vm.LoadCommand.CanExecute(null))
+                _vm.LoadCommand.Execute(null);
+        }
+    }
+}
