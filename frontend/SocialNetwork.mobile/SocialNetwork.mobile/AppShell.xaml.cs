092a11209c9931ead3089e1072437037fffcb8c8
Den1s-coder <diarusfoxs@gmail.com>
2025-11-08 20:55:33 +0200

    chore: add Route to Profile

--- frontend/SocialNetwork.mobile/SocialNetwork.mobile/AppShell.xaml.cs
+++ frontend/SocialNetwork.mobile/SocialNetwork.mobile/AppShell.xaml.cs
@@ -1,18 +1,20 @@
-﻿using SocialNetwork.mobile.Views;
+﻿using SocialNetwork.mobile.ViewModels;
+using SocialNetwork.mobile.Views;
 using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace SocialNetwork.mobile
 {
-    public partial class AppShell : Shell
+    public partial class AppShell : Xamarin.Forms.Shell
     {
         public AppShell()
         {
             InitializeComponent();
-
             Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
             Routing.RegisterRoute(nameof(PostsPage), typeof(PostsPage));
             Routing.RegisterRoute(nameof(PostDetailPage), typeof(PostDetailPage));
+            Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
