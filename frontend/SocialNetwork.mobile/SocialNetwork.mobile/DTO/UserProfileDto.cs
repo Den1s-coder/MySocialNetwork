f1fa01a9b5957ba52cc3ad9dea6fa63699aeb515
Den1s-coder <diarusfoxs@gmail.com>
2025-11-08 20:54:23 +0200

    chore: add user profile model

---
+++ frontend/SocialNetwork.mobile/SocialNetwork.mobile/DTO/UserProfileDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SocialNetwork.mobile.DTO
+{
+    public class UserProfileDto
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
---
+++ frontend/SocialNetwork.mobile/SocialNetwork.mobile/Models/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.mobile.Models
+{
+    public class UserProfile
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
