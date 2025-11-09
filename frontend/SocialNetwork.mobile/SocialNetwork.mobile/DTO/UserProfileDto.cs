3f95a1cd2f4e712207ff8496d688645e6b2c4d1a
Den1s-coder <diarusfoxs@gmail.com>
2025-11-09 11:46:53 +0200

    chore: add Dto for chat

---
+++ frontend/SocialNetwork.mobile/SocialNetwork.mobile/DTO/UserProfileDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.mobile.DTO
+{
+    public class UserProfileDto
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
