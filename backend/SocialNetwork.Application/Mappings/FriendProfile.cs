b0b844c992dcef77875cf4c970eedcdb09747491
Den1s-coder <diarusfoxs@gmail.com>
2025-12-16 14:49:13 +0200

    feat: add FriendService and Dto

---
+++ backend/SocialNetwork.Application/Mappings/FriendProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.Mappings
+{
+    public class FriendProfile : Profile
+    {
+        public FriendProfile()
+        {
+            //TODO
+        }
+    }
+}
---
+++ backend/SocialNetwork.Application/Service/FriendService.cs
@@ -0,0 +1,48 @@
+﻿using SocialNetwork.Application.DTO;
+using SocialNetwork.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.Service
+{
+    public class FriendService : IFriendService
+    {
+        public Task AcceptFriendRequest(Guid requestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeclineFriendRequest(Guid requestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserDto>> GetAllFriends()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserDto>> GetFriendsOfUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<FriendRequestDto>> GetPendingFriendRequests()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveFriend(Guid friendId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendFriendRequest(Guid addresseeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
