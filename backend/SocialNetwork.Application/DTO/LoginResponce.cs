40c9cf8e2d633a5d72f1c050133dcef03706b198
Den1s-coder <diarusfoxs@gmail.com>
2025-12-25 13:19:04 +0200

    chore: add Login responce with tokens

---
+++ backend/SocialNetwork.Application/DTO/LoginResponce.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.DTO
+{
+    public record LoginResponce
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+        public LoginResponce(string accessToken, string refreshToken)
+        {
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+    }
+}
