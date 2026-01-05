278d027b9b7b48baa1c1b2381e6a177541d653b0
Den1s-coder <diarusfoxs@gmail.com>
2026-01-05 12:49:51 +0200

    feat: add createCommentEvent

---
+++ backend/SocialNetwork.Application/Events/CommentCreatedEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace SocialNetwork.Application.Events
+{
+    public class CommentCreatedEventHandler : INotificationHandler<CommentCreatedEvent>
+    {
+        private readonly ILogger<CommentCreatedEventHandler> _logger;
+        public CommentCreatedEventHandler(ILogger<CommentCreatedEventHandler> logger)
+        {
+            _logger = logger;
+        }
+        public Task Handle(CommentCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Comment created with ID: {notification.CommentId} on Post ID: {notification.PostId} at {notification.CreatedAt} \n Notify post author");
+            return Task.CompletedTask;
+        }
+    }
+}
