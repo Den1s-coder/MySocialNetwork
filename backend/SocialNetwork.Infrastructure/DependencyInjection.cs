a8ec85db90f8985dd1c839c4f9a46c09cba728cd
Den1s-coder <diarusfoxs@gmail.com>
2026-01-05 15:25:04 +0200

    chore: DI in layers

---
+++ backend/SocialNetwork.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SocialNetwork.Domain.Interfaces;
+using SocialNetwork.Infrastructure.Repos;
+using SocialNetwork.Infrastructure.Security;
+
+namespace SocialNetwork.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IChatRepository, ChatRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<IFriendshipRepository, FriendshipRepository>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+
+            services.Configure<JwtOptions>(configuration.GetSection("JwtOptions"));
+            services.AddScoped<IJwtProvider, JwtProvider>();
+
+            return services;
+        }
+    }
+}
