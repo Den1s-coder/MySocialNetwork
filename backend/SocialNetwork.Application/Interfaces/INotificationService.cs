3820abf049035d84ee08440af6777ec9b9243ddc
Den1s-coder <diarusfoxs@gmail.com>
2026-01-27 15:26:05 +0200

    feat: add notification service

---
+++ backend/SocialNetwork.Application/Interfaces/INotificationService.cs
@@ -0,0 +1,16 @@
+﻿using SocialNetwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Application.Interfaces
+{
+    public interface INotificationService
+    {
+        Task SendNotificationAsync(Notification notification, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Notification>> GetUserNotificationsAsync(Guid userId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Notification>> GetUnreadNotificationsAsync(Guid userId, CancellationToken cancellationToken = default);
+    }
+}
