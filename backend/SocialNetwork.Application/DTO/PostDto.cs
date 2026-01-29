8225e96979442f02a806601bb7d696e6877f6977
Den1s-coder <diarusfoxs@gmail.com>
2026-01-29 17:16:01 +0200

    chore: update post DTO

--- backend/SocialNetwork.Application/DTO/PostDto.cs
+++ backend/SocialNetwork.Application/DTO/PostDto.cs
@@ -9,6 +9,7 @@ namespace SocialNetwork.Application.DTO
     public record PostDto
     {
         public Guid Id { get; set; }
+        public Guid AuthorId { get; set; }
         public string Text { get; set; }
         public string? ImageUrl { get; set; }
         public string UserName { get; set; }
