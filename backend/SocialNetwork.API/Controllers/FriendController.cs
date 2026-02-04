2a47623bd71cf915d86460ccbc09b16a256e9058
Den1s-coder <diarusfoxs@gmail.com>
2026-02-04 12:48:58 +0200

    feat: update user controller for change password and email (#118)
    Связанные рабочие элементы: #11

--- backend/SocialNetwork.API/Controllers/FriendController.cs
+++ backend/SocialNetwork.API/Controllers/FriendController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using SocialNetwork.Application.DTO;
+using SocialNetwork.Application.DTO.Users;
 using SocialNetwork.Application.Interfaces;
 using System.Security.Claims;
 
