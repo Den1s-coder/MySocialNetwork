a10424b2326a698aba10e6ea46a4efb050f3de71
Den1s-coder <diarusfoxs@gmail.com>
2026-02-16 15:30:42 +0200

    feat: reaction configurations

--- backend/SocialNetwork.Infrastructure/SocialDbContext.cs
+++ backend/SocialNetwork.Infrastructure/SocialDbContext.cs
@@ -3,6 +3,7 @@ using SocialNetwork.Domain.Entities;
 using SocialNetwork.Domain.Entities.Chats;
 using SocialNetwork.Domain.Entities.Comments;
 using SocialNetwork.Domain.Entities.Posts;
+using SocialNetwork.Domain.Entities.Reactions;
 using SocialNetwork.Domain.Entities.Users;
 using SocialNetwork.Infrastructure.Configurations;
 
@@ -25,6 +26,10 @@ namespace SocialNetwork.Infrastructure
         public DbSet<Friendship> Friendships { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
         public DbSet<Notification> Notifications { get; set; }
+        public DbSet<ReactionType> ReactionTypes { get; set; }
+        public DbSet<PostReaction> PostReactions { get; set; }
+        public DbSet<CommentReaction> CommentReactions { get; set; }
+        public DbSet<MessageReaction> MessageReactions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -37,6 +42,10 @@ namespace SocialNetwork.Infrastructure
             modelBuilder.ApplyConfiguration(new FriendshipConfiguration());
             modelBuilder.ApplyConfiguration(new RefreshTokenConfiguration());
             modelBuilder.ApplyConfiguration(new NotificationConfiguration());
+            modelBuilder.ApplyConfiguration(new ReactionConfiguration());
+            modelBuilder.ApplyConfiguration(new PostReactionConfiguration());
+            modelBuilder.ApplyConfiguration(new CommentReactionConfiguration());
+            modelBuilder.ApplyConfiguration(new MessageReactionConfiguration());
         }
     }
 }
