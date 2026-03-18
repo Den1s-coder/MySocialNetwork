360686924754f84ff5b71ae5ac608c4158404b43
Den1s-coder <diarusfoxs@gmail.com>
2026-03-18 14:43:08 +0200

    feat: add userId in GetMessage in Message service
    -GetMessageByChatIdAsync now accepts userId  for definition user reaction on message.

--- backend/SocialNetwork.Application/Interfaces/IMessageService.cs
+++ backend/SocialNetwork.Application/Interfaces/IMessageService.cs
@@ -1,11 +1,10 @@
 ﻿using SocialNetwork.Application.DTO.Chats;
-using SocialNetwork.Domain.Entities;
 
 namespace SocialNetwork.Application.Interfaces
 {
     public interface IMessageService
     {
-        public Task<IEnumerable<MessageDto>> GetMessageByChatIdAsync(Guid chatid, CancellationToken cancellationToken = default);
+        public Task<IEnumerable<MessageDto>> GetMessageByChatIdAsync(Guid chatId, Guid userId, CancellationToken cancellationToken = default);
         public Task ToogleReactionAsync(Guid messageId, Guid userId, Guid reactionType, CancellationToken cancellationToken = default);
     }
 }
