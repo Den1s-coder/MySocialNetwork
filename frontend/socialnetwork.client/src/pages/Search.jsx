c822246e8b399e2a30efe807b309dc0c0148a518
Den1s-coder <diarusfoxs@gmail.com>
2026-06-18 14:35:07 +0300

    fix: error  'useNavigate' is defined but never used.

--- frontend/socialnetwork.client/src/pages/Search.jsx
+++ frontend/socialnetwork.client/src/pages/Search.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useSearchParams} from 'react-router-dom';
 import { authFetch } from '../hooks/authFetch';
 import Avatar from '../components/Avatar';
 import { Link } from 'react-router-dom';
